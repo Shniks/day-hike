8c47680fdba1eb569d428961c6fba6fe72daaeec
Nikhil Shahi <shniks@gmail.com>
2022-08-04 14:58:58 -0700

    Fixed error in the relationships statements for the feature tests.

--- spec/models/trail_spec.rb
+++ spec/models/trail_spec.rb
@@ -9,6 +9,6 @@ RSpec.describe Trail, type: :model do
 
   describe 'relationships' do
     it { should have_many :trip_trails }
-    it { should have_many(trips).through(:trip_trails) }
+    it { should have_many(:trips).through(:trip_trails) }
   end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Trip, type: :model do
 
   describe 'relationships' do
     it { should have_many :trip_trails }
-    it { should have_many(trails).through(:trip_trails) }
+    it { should have_many(:trails).through(:trip_trails) }
   end
 end
