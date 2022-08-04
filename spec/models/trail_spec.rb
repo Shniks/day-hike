15b5b88f69d921ad3c7efcee9d2a357ba41657f4
Nikhil Shahi <shniks@gmail.com>
2022-08-04 14:46:22 -0700

    Created model test for the trail model.

---
+++ spec/models/trail_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Trail, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :address }
+    it { should validate_presence_of :location }
+  end
+end
