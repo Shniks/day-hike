e7f7e39e1b374fa0138609714d62970d25083c97
Nikhil Shahi <shniks@gmail.com>
2022-08-04 16:55:49 -0700

    Created trip feature test for user story 2. To find the total hiking distance.

---
+++ spec/features/trips/user_can_see_total_hiking_distance_in_trip_show_spec.rb
@@ -0,0 +1,24 @@
+equire 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  describe 'When I visit a trip show page' do
+    it 'I can see the planned total hiking distance' do
+
+      trip_1 = Trip.create!(name: "Early August")
+      trip_2 = Trip.create!(name: "Mid August")
+
+      trail_1 = Trail.create!(name: "Mist Trail", address: "Mist Trail Road, Yosemite Valley", length: 7.5)
+      trail_2 = Trail.create!(name: "4 Mile Trail", address: "4 Mile Trailhead, Yosemite Valley", length: 12.0)
+      trail_3 = Trail.create!(name: "Mirror Lake Loop", address: "Mirror Lake, Yosemite Valley", length: 4.6)
+
+      TripTrail.create!(trip: trip_1, trail: trail_1)
+      TripTrail.create!(trip: trip_1, trail: trail_2)
+      TripTrail.create!(trip: trip_2, trail: trail_3)
+      TripTrail.create!(trip: trip_2, trail: trail_1)
+
+      visit '/trips/2'
+
+      expect(page).to have_content('Planned Total Hiking Distance: 12.1 miles')
+    end
+  end
+end
