d4fcd7b49ef9b10caed91d1eb13f93722ea3c2cb
Nikhil Shahi <shniks@gmail.com>
2022-08-04 17:21:36 -0700

    Added model test to trip model to find average hiking distance.

--- spec/models/trip/trip_spec.rb
+++ spec/models/trip/trip_spec.rb
@@ -11,16 +11,22 @@ RSpec.describe Trip, type: :model do
   end
 
   describe 'calculations' do
-    it 'can find the total hiking distance' do
-      trip = Trip.create!(name: "Early August")
+    before do
+      @trip = Trip.create!(name: "Early August")
 
       trail_1 = Trail.create!(name: "Mist Trail", address: "Mist Trail Road, Yosemite Valley", length: 7.5)
       trail_2 = Trail.create!(name: "4 Mile Trail", address: "4 Mile Trailhead, Yosemite Valley", length: 12.0)
 
-      TripTrail.create!(trip: trip, trail: trail_1)
-      TripTrail.create!(trip: trip, trail: trail_2)
+      TripTrail.create!(trip: @trip, trail: trail_1)
+      TripTrail.create!(trip: @trip, trail: trail_2)
+    end
 
-      expect(trip.total_hiking_distance).to eq(19.5)
+    it 'can find the total hiking distance' do
+      expect(@trip.total_hiking_distance).to eq(19.5)
+    end
+
+    it 'can find the average hiking distance' do
+      expect(@trip.average_hiking_distance).to eq(0.975e1)
     end
   end
 
